0a1396da0bb7866edf81190417f2cb8395efefac
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-07-31 00:10:17 +0530

    Code Optimization...

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import { Component } from "react";
 // import ComponentA from "./Components/Component/ComponentA";
 import Timer from "./Components/Timer/TimerOne";
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      mount: false,
+    };
+  }
+  handleMount = () => {
+    this.setState((prevState) => ({ mount: !prevState.mount }));
+  };
   render() {
     return (
       <>
         {/* <ComponentA /> */}
-        <Timer />
+        <button onClick={this.handleMount}>
+          {this.state.mount ? "Un-Mount" : "Mount"}
+        </button>
+        {this.state.mount ? <Timer /> : null}
       </>
     );
   }
