c22cabf91db110f231754b92224ed3993c8f72b0
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-07-30 17:30:36 +0530

    Code Optimization...

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -8,6 +8,7 @@ export default class ComponentA extends Component {
     this.state = {
       name: "ComponentA",
       data: [],
+      loading: false,
     };
     console.log("ComponentA in Constructor...");
   }
@@ -19,44 +20,49 @@ export default class ComponentA extends Component {
 
   componentDidMount() {
     console.log("ComponentA in componentDidMount");
+    this.setState({ loading: true });
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((data) => this.setState({ data }));
+      .then((data) => this.setState({ data }))
+      .finally(() => this.setState({ loading: false }));
   }
 
   render() {
     console.log("ComponentA in render...");
-    console.log(this.state.data);
-    return (
-      <>
-        <h1>{this.state.name}</h1>
-        <h2>Users Data-Table</h2>
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <>
+          <h1>{this.state.name}</h1>
+          <h2>Users Data-Table</h2>
 
-        <table style={{ border: "1px solid black" }}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Company</th>
-              <th>Address</th>
-              <th>Website</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.data.map((d, index) => {
-              return (
-                <tr key={index}>
-                  <td className="cell-blue">{d.name}</td>
-                  <td className="cell-green">{d.email}</td>
-                  <td className="cell-purple">{d.company.name}</td>
-                  <td className="cell-blue">{d.address.street}</td>
-                  <td className="cell-green">{d.website}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
+          <table style={{ border: "1px solid black" }}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Company</th>
+                <th>Address</th>
+                <th>Website</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((d, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="cell-blue">{d.name}</td>
+                    <td className="cell-green">{d.email}</td>
+                    <td className="cell-purple">{d.company.name}</td>
+                    <td className="cell-blue">{d.address.street}</td>
+                    <td className="cell-green">{d.website}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      );
+    }
   }
 }
