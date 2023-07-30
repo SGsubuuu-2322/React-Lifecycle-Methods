1f7e2558affe55b3971b00d62ab26696b96af54b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-07-30 23:16:16 +0530

    Code Optimization...

--- src/Components/Timer/TimerOne.js
+++ src/Components/Timer/TimerOne.js
@@ -4,8 +4,9 @@ export default class Timer extends Component {
   constructor() {
     super();
     this.state = {
-      value: 0,
+      time: 0,
     };
+    this.timer = null;
     console.log("TimerOne constructor method...");
   }
 
@@ -19,19 +20,23 @@ export default class Timer extends Component {
     return true;
   }
 
-  incValue = () => {
-    this.setState((prevState) => {
-      return {
-        value: prevState.value + 1,
-      };
-    });
-  };
+  //   incValue = () => {
+  //     this.setState((prevState) => {
+  //       return {
+  //         value: prevState.value + 1,
+  //       };
+  //     });
+  //   };
   render() {
     console.log("TimerOne render method...");
     return (
       <>
-        <h1>Value: {this.state.value}</h1>
-        <button onClick={this.incValue}>Click For Updation</button>
+        {/* <h1>Value: {this.state.value}</h1> */}
+        {/* <button onClick={this.incValue}>Click For Updation</button> */}
+        <h1>
+          Time-Spent:{" "}
+          {new Date(this.state.time * 1000).toISOString().slice(11, 19)}
+        </h1>
       </>
     );
   }
@@ -39,6 +44,12 @@ export default class Timer extends Component {
   componentDidMount() {
     console.log("TimerOne componentDidMount method...");
     console.log("-------------------------------------");
+
+    this.timer = setInterval(() => {
+      this.setState((prevState) => ({
+        time: prevState.time + 1,
+      }));
+    }, 1000);
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
@@ -49,5 +60,13 @@ export default class Timer extends Component {
   componentDidUpdate() {
     console.log("Timer componentDidUpdate method...");
     console.log("-------------------------------------");
+  }
+
+  componentWillUnmount() {
+    console.log("Timer componentWillUnmount method...");
+    console.log("-------------------------------------");
+
+    // Clear the interval when the component is unmounted
+    clearInterval(this.timer);
   }
 }
